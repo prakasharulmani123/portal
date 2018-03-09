3249164cb30ec08b7e84d7e874be623d27ea7210
prakasharulmani <prakash.paramanandam@arkinfotec.com>
2018-03-09 19:19:58 +0530

    Code optimize

--- app/Controller/CompensationsController.php
+++ app/Controller/CompensationsController.php
@@ -1,18 +1,22 @@
 <?php
-class CompensationsController extends AppController {
-	public $name='Compensations';
+
+class CompensationsController extends AppController
+{
+    public $name = 'Compensations';
     public $components = array('RequestHandler');
-    public function get_id($compensation_id = NULL){
-         		return $this->Compensation->find('first', array('recursive'=>-1,'conditions'=>array('Compensation.id' => $compensation_id)));
-	}
-         public function add_current_month_permission_new() {
-             print_r('haiiii');
-             exit;
-             return $this->Compensation->find('count', array('conditions' => array('AND' => array('Compensation.user_id=' . $this->Session->read('User.id')), array('Compensation.status' => 0),array('Compensation.type' => 'P'))));
-//pr($return);exit;
+
+    public function get_id($compensation_id = NULL)
+    {
+        return $this->Compensation->find('first', array('recursive' => -1, 'conditions' => array('Compensation.id' => $compensation_id)));
     }
-public function get_permission_id($compensation_id = NULL){
-         		return $this->Compensation->find('first', array('recursive'=>-1,'conditions'=>array('Compensation.id' => $compensation_id)));
-	}
+
+    public function add_current_month_permission_new()
+    {
+        return $this->Compensation->find('count', array('conditions' => array('AND' => array('Compensation.user_id=' . $this->Session->read('User.id')), array('Compensation.status' => 0), array('Compensation.type' => 'P'))));
+    }
+
+    public function get_permission_id($compensation_id = NULL)
+    {
+        return $this->Compensation->find('first', array('recursive' => -1, 'conditions' => array('Compensation.id' => $compensation_id)));
+    }
 }
-    ?>
